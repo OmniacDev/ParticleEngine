035ebdb751d98d02482357805c9bc5ecce54f150
Omniac <blueleafmonkey.gaming@gmail.com>
2024-08-18 01:08:07 +1200

    reimpl QuadTree.h

--- Physics/Solver.h
+++ Physics/Solver.h
@@ -3,7 +3,7 @@
 #include <vector>
 
 #include "Particle.h"
-#include "QuadTree/BetterQuadTree.h"
+#include "QuadTree/QuadTree.h"
 #include "../Engine/EngineGlobalVars.h"
 
 namespace SOLVER {
@@ -11,7 +11,7 @@ namespace SOLVER {
 
     inline int COLLISION_COUNT = 0;
 
-    inline QuadTree QuadTree(Rect(FVector2(BOUNDS::X_NEG, BOUNDS::Y_NEG), FVector2((float)GRID::Amount.X * GRID::Size, (float)GRID::Amount.Y * GRID::Size)));
+    inline Quad::Tree QuadTree(Rect(FVector2(BOUNDS::X_NEG, BOUNDS::Y_NEG), FVector2((float)GRID::Amount.X * GRID::Size, (float)GRID::Amount.Y * GRID::Size)));
 
     void SolveCollision(Particle& One, Particle& Two);
 }
