757b15fbab91e5962cf894261432529e2ef4476c
Omniac <blueleafmonkey.gaming@gmail.com>
2024-09-07 01:01:31 +1200

    stuff

--- main.cpp
+++ main.cpp
@@ -14,21 +14,21 @@
 #include "Engine/SFML/ShapeConversions.h"
 // #include "Engine/SFML/VertexArray.h"
 
-#include "Engine/Shaders/Shaders.h"
+// #include "Engine/Shaders/Shaders.h"
 
-bool FAST_RENDERING = false;
-bool RENDERING_ENABLED = true;
+//bool FAST_RENDERING = false;
+//bool RENDERING_ENABLED = true;
 
 float AVG_FPS = 0.f;
 
 std::deque<float> FPS_Queue;
 
-const FVector2 SearchSize (100.f, 100.f);
+//const FVector2 SearchSize (100.f, 100.f);
 
 int main()
 {
-    sf::Shader particle_shader;
-    particle_shader.loadFromMemory(particle_vs, particle_fs);
+//    sf::Shader particle_shader;
+//    particle_shader.loadFromMemory(particle_vs, particle_fs);
 
     sf::ContextSettings window_settings;
     window_settings.antialiasingLevel = 8;
@@ -65,7 +65,7 @@ int main()
         }
     }
 
-    IVector2 last_mouse_pos = IVector2(0, 0);
+    // IVector2 last_mouse_pos = IVector2(0, 0);
 
     while (window.isOpen())
     {
@@ -154,7 +154,7 @@ int main()
             window.draw(line, 2, sf::Lines);
         }
 
-        for (int substep = 0; substep < 8; substep++) {
+        for (int substep = 0; substep < 12; substep++) {
             for (int i = 0; i < SOLVER::QuadTree.nodes.Range(); i++) {
                 if (SOLVER::QuadTree.nodes[i].num == -1) continue;
 
@@ -212,24 +212,24 @@ int main()
             particle.setOutlineThickness(1.0f);
             particle.setOutlineColor({Particle.color.r, Particle.color.g, Particle.color.b, 255});
 
-            const int vi = i * 4;
+//            const int vi = i * 4;
+//
+//            vertices[vi + 0].position = {(float)ParticleWindowLocation.X - Particle.radius, (float)ParticleWindowLocation.Y - Particle.radius};
+//            vertices[vi + 1].position = {(float)ParticleWindowLocation.X + Particle.radius, (float)ParticleWindowLocation.Y - Particle.radius};
+//            vertices[vi + 2].position = {(float)ParticleWindowLocation.X + Particle.radius, (float)ParticleWindowLocation.Y + Particle.radius};
+//            vertices[vi + 3].position = {(float)ParticleWindowLocation.X - Particle.radius, (float)ParticleWindowLocation.Y + Particle.radius};
+//
+//            vertices[vi + 0].texCoords = { 0.f, 0.f };
+//            vertices[vi + 1].texCoords = { 1.f, 0.f };
+//            vertices[vi + 2].texCoords = { 1.f, 1.f };
+//            vertices[vi + 3].texCoords = { 0.f, 1.f };
+//
+//            vertices[vi + 0].color = {Particle.color.r, Particle.color.g, Particle.color.b, 127};
+//            vertices[vi + 1].color = {Particle.color.r, Particle.color.g, Particle.color.b, 127};
+//            vertices[vi + 2].color = {Particle.color.r, Particle.color.g, Particle.color.b, 127};
+//            vertices[vi + 3].color = {Particle.color.r, Particle.color.g, Particle.color.b, 127};
 
-            vertices[vi + 0].position = {(float)ParticleWindowLocation.X - Particle.radius, (float)ParticleWindowLocation.Y - Particle.radius};
-            vertices[vi + 1].position = {(float)ParticleWindowLocation.X + Particle.radius, (float)ParticleWindowLocation.Y - Particle.radius};
-            vertices[vi + 2].position = {(float)ParticleWindowLocation.X + Particle.radius, (float)ParticleWindowLocation.Y + Particle.radius};
-            vertices[vi + 3].position = {(float)ParticleWindowLocation.X - Particle.radius, (float)ParticleWindowLocation.Y + Particle.radius};
-
-            vertices[vi + 0].texCoords = { 0.f, 0.f };
-            vertices[vi + 1].texCoords = { 1.f, 0.f };
-            vertices[vi + 2].texCoords = { 1.f, 1.f };
-            vertices[vi + 3].texCoords = { 0.f, 1.f };
-
-            vertices[vi + 0].color = {Particle.color.r, Particle.color.g, Particle.color.b, 127};
-            vertices[vi + 1].color = {Particle.color.r, Particle.color.g, Particle.color.b, 127};
-            vertices[vi + 2].color = {Particle.color.r, Particle.color.g, Particle.color.b, 127};
-            vertices[vi + 3].color = {Particle.color.r, Particle.color.g, Particle.color.b, 127};
-
-            // window.draw(particle);
+            window.draw(particle);
 
 //            window.draw(CircleToVertices(particle));
 //
@@ -237,8 +237,8 @@ int main()
 //            CombineIntoVertexArray(CircleOutlineToVertices(particle), outline_vertices);
         }
 
-        particle_shader.setUniform("time", SafeDeltaTime);
-        window.draw(vertices);
+//        particle_shader.setUniform("time", SafeDeltaTime);
+//        window.draw(vertices);
 
 //         window.draw(vertices);
 //         window.draw(outline_vertices);
